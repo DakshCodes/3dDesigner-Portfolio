de811d9df7a21ea765c3fd9044e83ba25c91a4ec
dakshsinghk2524@gmail.com[200~you@example.com~ <you@example.com>
2023-11-16 20:53:18 +0530

    Add Optimize-2

--- src/pages/Home/animations.js
+++ src/pages/Home/animations.js
@@ -15,14 +15,15 @@ const animateHome = () => {
     });
 
     gsap.set(".hero-imges img", { opacity: 0 });
+    gsap.set(".container .nav-toggle-btn", { x: 40, opacity: 0 });
+    gsap.set(".container #logo", { opacity: 0, x: -40 });
 
     const gTl = gsap.timeline();
 
-    gTl.from(".container .logo", 1, { delay: 4, opacity: 1, x: -100, ease: "back.out" });
-    gTl.from(".container .nav-toggle-btn", 1, { x: -100, ease: "back.out" });
+    gTl.to(".container #logo",{ delay: 4.2, opacity: 1, x: 0, ease: "back.out" }).to(".container .nav-toggle-btn", { delay: -0.2,x: 0, opacity: 1, ease: "back.out" });
     gTl.from(".hero-banner", 2, { opacity: 0, xPercent: -130, delay: -2.2, ease: "expo.out" });
-    gTl.from(".hero-title .char", 1, { opacity: 0, yPercent: 130, stagger: 0.06, delay: -1.5, ease: "back.out" });
-    gTl.from(".hero-title2 .char", 1, { opacity: 0, xPercent: 130, stagger: 0.06, delay: -1, ease: "back.out" });
+    gTl.from(".hero-title .char", 1, { opacity: 0, yPercent: 130, stagger: 0.06, delay: -1, ease: "back.out" });
+    gTl.from(".hero-title2 .char", 1, { opacity: 0, xPercent: 130, stagger: 0.06, delay: -0.7, ease: "back.out" });
 
     gTl.to('.hero-imges img', 0.35, { opacity: 1, delay: -1, clipPath: "polygon(50% 0%, 51% 0%, 51% 100%, 50% 100%)" })
         .to('.hero-imges img', 0.4, { opacity: 1, clipPath: "polygon(40% 0%, 51% 0%, 51% 100%, 40% 100%)" })
