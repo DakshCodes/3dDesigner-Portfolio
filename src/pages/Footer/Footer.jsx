e7c9367a814c5009eb0fe82c181a4a17bc634cdf
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-11-27 14:15:47 +0530

    Some bug fixes

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -43,11 +43,11 @@ export default function Footer() {
                         transition={{ duration: 0.4, delay: 0.2 }}
                     >together</motion.h2>
                     <motion.div style={{ x }} className="buttonContainer">
-                        <Rounded backgroundColor={"red"} className="button">
-                            <Link to={'/contact'}>
+                        <Link to={'/contact'}>
+                            <Rounded backgroundColor={"red"} className="button">
                                 <p>Get in touch</p>
-                            </Link>
-                        </Rounded>
+                            </Rounded>
+                        </Link>
                     </motion.div>
                     <motion.svg
                         initial={{ opacity: 0, x: -40 }}
@@ -57,7 +57,7 @@ export default function Footer() {
                         <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white" />
                     </motion.svg>
                 </div>
-                <div className="nav">
+                <div className="nav 2xl:mt-[200px]">
                     <motion.div
                         initial={{ opacity: 0, y: 40 }}
                         whileInView={{ opacity: 1, y: 0, }}
@@ -70,7 +70,7 @@ export default function Footer() {
                         whileInView={{ opacity: 1, y: 0, }}
                         transition={{ duration: 0.4, delay: 0.2 }}
                         className='roundedButton-1'>
-                        <p>+31 xxx 80 xx</p>
+                        <p>94600 09410</p>
                     </motion.div>
                 </div>
                 <div className="info">
@@ -107,16 +107,14 @@ export default function Footer() {
                             >Socials</motion.h3>
                         </motion.span>
                         <Magnetic>
-                            <p>Awwwards</p>
+                            <Link to={"https://www.instagram.com/itx3dstudio/"} target='_blank'>
+                                <p>Instagram</p>
+                            </Link>
                         </Magnetic>
                         <Magnetic>
-                            <p>Instagram</p>
-                        </Magnetic>
-                        <Magnetic>
-                            <p>Dribble</p>
-                        </Magnetic>
-                        <Magnetic>
+                            <Link to={"https://www.linkedin.com/in/ankit-kumawat-11560a218/"} target='_blank'>
                             <p>Linkedin</p>
+                            </Link>
                         </Magnetic>
                     </div>
                 </div>
