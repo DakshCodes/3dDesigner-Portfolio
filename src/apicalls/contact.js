1dd93e8738bd7e0bfff05c6b4309f59c94b98c44
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-11-30 22:29:44 +0530

    added the contact section to server

--- src/apicalls/contact.js
+++ src/apicalls/contact.js
@@ -2,7 +2,7 @@ import axios from "axios"
 // import toast from "react-hot-toast"
 export const RegisterUser = async(payload)=>{
     try {
-        const response = await axios.post("http://localhost:8080/api/sendMail",payload);
+        const response = await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/sendMail`,payload);
         return response.data;
     //    response.data = toast.success('email sent sucessfully');
     } catch (error) {
